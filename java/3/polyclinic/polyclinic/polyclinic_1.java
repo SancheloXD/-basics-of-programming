b34d3d529f2ffb8b333a72ea672cfcd9e960917a
SancheloXD <98591807+SancheloXD@users.noreply.github.com>
2022-06-16 21:47:00 +0300

    1

---
+++ java/3/polyclinic/polyclinic/polyclinic_1.java
@@ -0,0 +1,13 @@
+/*
+    Номер зачетной книжки: 21-670.
+    Автор: Иванников Александр Владимирович, ЗИТ-21
+    Вариант-13. Polyclinic
+*/
+package polyclinic;
+
+public class polyclinic_1 {
+    public static void main(String[] args) {
+       polyclinic polyclinic = new polyclinic ("3 поликлиника","Труда 8","Иваннникв","132456","16.06.2022","Смирнов","Терапевт","Здоров");
+       System.out.println(polyclinic);
+   }
+}
